1456b122e2043ceb1865319f688193d0e8847713
cfis <cfis@savagexi.com>
2026-01-04 00:41:43 -0800

    Fix test.

--- test/core_test.rb
+++ test/core_test.rb
@@ -40,7 +40,11 @@ class CoreTest < OpenCVTestCase
     end
     values, vectors = mat1.eigen
     assert_equal([8.561553001403809, 4.438446998596191], values.to_a)
-    assert_equal([0.6154122352600098, -0.7882053852081299, -0.7882053852081299, -0.6154122352600098], vectors.to_a)
+    if Gem.win_platform?
+      assert_equal([-0.615412175655365, 0.7882053852081299, 0.7882053852081299, 0.615412175655365], vectors.to_a)
+    else
+      assert_equal([0.6154122352600098, -0.7882053852081299, -0.7882053852081299, -0.6154122352600098], vectors.to_a)
+    end
   end
 
   def test_flip

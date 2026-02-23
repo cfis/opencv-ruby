b61f049fd65b08e7f69de399f28c5f4f50d8bbaf
cfis <cfis@savagexi.com>
2026-02-22 22:59:22 -0800

    Remove debug code.

--- lib/opencv-ruby.rb
+++ lib/opencv-ruby.rb
@@ -1,18 +1,14 @@
 if RUBY_PLATFORM =~ /mingw|mswin/
   major, minor, _ = RUBY_VERSION.split(".")
   lib_path = File.join(__dir__)
-
-  #$LOAD_PATH.unshift(ext_path)
   path = File.join(lib_path, "#{major}.#{minor}")
   cuda_path = File.join(ENV["CUDA_PATH"], "bin")
   ENV["PATH"] = "#{path}#{File::PATH_SEPARATOR}#{cuda_path}#{File::PATH_SEPARATOR}#{ENV["PATH"]}"
-  #ENV['CUDA_VISIBLE_DEVICES'] = "-1"
-  #ENV['OPENCV_CUDA_DEVICE'] = "-1"
-  #begin
+  begin
     require "#{major}.#{minor}/opencv_ruby.so"
-    #  rescue LoadError => e
-    #require "opencv_ruby"
-    #end
+  rescue LoadError => e
+    require "opencv_ruby"
+  end
 else
   require "opencv_ruby"
 end
